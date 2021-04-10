57908a4d9cf987931123786cc7023abd310caa87
comp500 <comp500@users.noreply.github.com>
2021-04-10 01:45:54 +0100

    Start internal rewrite of file download system

--- src/main/kotlin/link/infra/packwiz/installer/UpdateManager.kt
+++ src/main/kotlin/link/infra/packwiz/installer/UpdateManager.kt
@@ -3,7 +3,6 @@ package link.infra.packwiz.installer
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonIOException
 import com.google.gson.JsonSyntaxException
-import com.google.gson.annotations.SerializedName
 import com.moandjiezana.toml.Toml
 import link.infra.packwiz.installer.DownloadTask.Companion.createTasksFromIndex
 import link.infra.packwiz.installer.metadata.IndexFile
@@ -15,6 +14,7 @@ import link.infra.packwiz.installer.metadata.hash.HashUtils.getHash
 import link.infra.packwiz.installer.metadata.hash.HashUtils.getHasher
 import link.infra.packwiz.installer.request.HandlerManager.getFileSource
 import link.infra.packwiz.installer.request.HandlerManager.getNewLoc
+import link.infra.packwiz.installer.target.Side
 import link.infra.packwiz.installer.ui.IUserInterface
 import link.infra.packwiz.installer.ui.IUserInterface.CancellationResult
 import link.infra.packwiz.installer.ui.IUserInterface.ExceptionListResult
@@ -29,7 +29,6 @@ import java.io.FileWriter
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.*
 import java.util.concurrent.CompletionService
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.ExecutorCompletionService
@@ -57,56 +56,6 @@ class UpdateManager internal constructor(private val opts: Options, val ui: IUse
 				Options(downloadURI, manifestFile ?: "packwiz.json", packFolder ?: ".", side ?: Side.CLIENT)
 		}
 
-		enum class Side {
-			@SerializedName("client")
-			CLIENT("client"),
-			@SerializedName("server")
-			SERVER("server"),
-			@SerializedName("both")
-			@Suppress("unused")
-			BOTH("both", arrayOf(CLIENT, SERVER));
-
-			private val sideName: String
-			private val depSides: Array<Side>?
-
-			constructor(sideName: String) {
-				this.sideName = sideName.toLowerCase()
-				depSides = null
-			}
-
-			constructor(sideName: String, depSides: Array<Side>) {
-				this.sideName = sideName.toLowerCase()
-				this.depSides = depSides
-			}
-
-			override fun toString() = sideName
-
-			fun hasSide(tSide: Side): Boolean {
-				if (this == tSide) {
-					return true
-				}
-				if (depSides != null) {
-					for (depSide in depSides) {
-						if (depSide == tSide) {
-							return true
-						}
-					}
-				}
-				return false
-			}
-
-			companion object {
-				fun from(name: String): Side? {
-					val lowerName = name.toLowerCase()
-					for (side in values()) {
-						if (side.sideName == lowerName) {
-							return side
-						}
-					}
-					return null
-				}
-			}
-		}
 	}
 
 	private fun start() {
@@ -445,4 +394,5 @@ class UpdateManager internal constructor(private val opts: Options, val ui: IUse
 			exitProcess(0)
 		}
 	}
+
 }
